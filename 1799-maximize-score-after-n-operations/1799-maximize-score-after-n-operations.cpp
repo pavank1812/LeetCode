24bfed56250919b2647d1c5ebfccd7c8113c8aa7
Pavan Anand Kunte <129414399+pavank1812@users.noreply.github.com>
2023-05-14 10:20:55 +0530

    Time: 294 ms (32.87%), Space: 8.9 MB (55.02%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
   int maxScore(vector<int>& nums) {
     const int l = nums.size();
-    vector<int> dp(1 << l);    
-    for (int mask = 0; mask < 1 << l; ++mask) {
-      int c = __builtin_popcount(mask);
-      if (c & 1) continue; // only do in pairs
-      int k = c / 2 + 1;
+    vector<int> cache(1 << l);
+    function<int(int)> dp = [&](int mask) {      
+      if (mask == 0) return 0;
+      int& ans = cache[mask];
+      if (ans > 0) return ans;
+      const int k = (l - __builtin_popcount(mask)) / 2 + 1;
       for (int i = 0; i < l; ++i)
         for (int j = i + 1; j < l; ++j)
-          if ((mask & (1 << i)) + (mask & (1 << j)) == 0) {            
-            int new_mask = mask | (1 << i) | (1 << j);            
-            dp[new_mask] = max(dp[new_mask],
-                               k * gcd(nums[i], nums[j]) + dp[mask]);
-        }
-    }
-    return dp[(1 << l) - 1];
+          if ((mask & (1 << i)) && (mask & (1 << j)))
+            ans = max(ans, k * gcd(nums[i], nums[j])
+                           + dp(mask ^ (1 << i) ^ (1 << j)));
+      return ans;
+    };
+    return dp((1 << l) - 1);
   }
 };
