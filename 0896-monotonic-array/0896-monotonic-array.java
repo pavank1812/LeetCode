3052224d554d31d7a0b9667b9061410d8e550605
Pavan Anand Kunte <129414399+pavank1812@users.noreply.github.com>
2023-09-29 08:36:02 +0530

    Time: 3 ms (43.67%), Space: 55.4 MB (73.99%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,28 @@
+class Solution {
+  public boolean isMonotonic(int[] nums) {
+    boolean increasing = true;
+    boolean decreasing = true;
+
+    for (int i = 1; i < nums.length; ++i) {
+      increasing &= nums[i] >= nums[i - 1];
+      decreasing &= nums[i] <= nums[i - 1];
+    }
+
+    return increasing || decreasing;
+  }
+}
+
+
+// class Solution {
+//     public boolean isMonotonic(int[] nums) {
+//         boolean increasing = true;
+//         boolean decreasing = false;
+        
+//         for(int i = 1; i < nums.length; ++i){
+//             increasing &= nums[i] >= nums[i-1];
+//             decreasing &= nums[i] <= nums[i-1];
+//         }
+//         return increasing || decreasing;
+//     }
+// }
+
