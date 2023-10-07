ace0fd5d7be0069c437113b7b743c240dded51ee
Pavan Anand Kunte <129414399+pavank1812@users.noreply.github.com>
2023-10-07 16:28:35 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (83.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0){
+        if(n==0){
             return 0;
-        }else if(n == 1){
+        }else if(n==1){
             return 1;
         }else{
             int a=0,b=1;
-            for(int i=2;i<=n;i++){
+            for(int i=2; i<=n; i++){
                 int temp = a+b;
                 a=b;
-                b=temp;
+                b = temp;
             }
             return b;
         }
     }
 }
+
+
