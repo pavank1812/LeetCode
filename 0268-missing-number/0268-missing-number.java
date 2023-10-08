76d8ddf0bc2f2c19f6d3c59cd925cdc4647b40ed
Pavan Anand Kunte <129414399+pavank1812@users.noreply.github.com>
2023-10-08 18:21:28 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (85.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -9,11 +9,11 @@
 //     }
 // }
 
-class Solution{
+class Solution {
     public int missingNumber(int[] nums){
         int n = nums.length;
         int totalSum = n*(n+1)/2;
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n;i++){
             totalSum -= nums[i];
         }
         return totalSum;
