86d0f178083fec8541c2c8d918fbd3c8a03b8cfa
Pavan Anand Kunte <129414399+pavank1812@users.noreply.github.com>
2024-03-06 09:29:58 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (87.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,19 +9,18 @@
  *     }
  * }
  */
+
 public class Solution {
-    public boolean hasCycle(ListNode head) {
+    public boolean hasCycle(ListNode head){
         ListNode slow = head;
         ListNode fast = head;
-        
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast)
                 return true;
         }
-        
-    return false;
+        return false; 
     }
 }
 
